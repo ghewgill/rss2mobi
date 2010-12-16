537ec5654f20b375a21374fb4f667e3f94b489db
Greg Hewgill <greg@hewgill.com>
2010-12-16 23:40:03 +1300

    download and include images in book

--- rss2mobi.py
+++ rss2mobi.py
@@ -1,11 +1,48 @@
 import ast
+import hashlib
+import html.parser
 import os
 import pprint
 import shutil
 import tempfile
 import time
+import urllib
 
 import greader
+
+g_Images = set()
+
+class ImageRewriter(html.parser.HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.output = ""
+        self.images = []
+    def handle_starttag(self, tag, attrs):
+        if tag == "img":
+            i = [x[0] for x in attrs].index("src")
+            src = attrs[i][1]
+            id = hashlib.sha1(src.encode("utf-8")).hexdigest()
+
+            print("fetching {}".format(src))
+            r = urllib.request.urlopen(src)
+            ct = r.getheader("Content-Type")
+            ext = {
+                "image/gif":  ".gif",
+                "image/jpeg": ".jpg",
+                "image/png":  ".png",
+            }[ct]
+            with open(os.path.join(dir, id + ext), "wb") as f:
+                f.write(r.read())
+
+            attrs[i] = (attrs[i][0], id + ext)
+            g_Images.add((id, id + ext, ct))
+            self.output += "<{0}{1}>".format(tag, "".join(' {0}="{1}"'.format(*x) for x in attrs))
+        else:
+            self.output += self.get_starttag_text()
+    def handle_endtag(self, tag):
+        self.output += "</{}>".format(tag)
+    def handle_data(self, data):
+        self.output += data
 
 with open("rss2mobi.config") as f:
     Config = ast.literal_eval(f.read())
@@ -30,10 +67,14 @@ try:
         print("<h1>{0}</h1>".format(e['origin']['title']), file=f)
         print("""<h2><a href="{0}">{1}</a></h2>""".format(e['alternate'][0]['href'], e['title']), file=f)
         which = "content" if "content" in e else "summary"
-        print(e[which]['content'], file=f)
+        content = e[which]['content']
+        rw = ImageRewriter()
+        rw.feed(content)
+        print(rw.output, file=f)
         print("</body>", file=f)
         print("</html>", file=f)
         f.close()
+
     opfn = "reader-{0}.opf".format(today)
     f = open(os.path.join(dir, opfn), "w")
     print("""<?xml version="1.0" encoding="UTF-8"?>
@@ -48,6 +89,8 @@ try:
         <manifest>""".format(today, len(feed['items'])), file=f)
     for e in feed['items']:
         print("""<item id="{0}" href="{0}.html" media-type="text/html" />""".format(e['id'].replace("/", "_")), file=f)
+    for i in g_Images:
+        print("""<item id="{0}" href="{1}" media-type="{2}" />""".format(*i), file=f)
     print("""</manifest>
         <spine>""", file=f)
     for e in feed['items']:
