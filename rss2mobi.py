15eefac2e57e12803c02690eb40080c535f896ba
Greg Hewgill <greg@hewgill.com>
2010-12-16 19:46:40 +1300

    create .html files so kindlegen reads them as utf-8

--- rss2mobi.py
+++ rss2mobi.py
@@ -20,16 +20,19 @@ os.mkdir(dir)
 try:
     for e in feed['items']:
         #pprint.pprint(e)
-        f = open(os.path.join(dir, e['id'].replace("/", "_")), "w")
-        print("<html>", file=f)
+        f = open(os.path.join(dir, e['id'].replace("/", "_"))+".html", "w", encoding="utf-8")
+        print("""<?xml version="1.0" encoding="UTF-8"?>""", file=f)
+        print("""<html xmlns="http://www.w3.org/1999/xhtml>""", file=f)
         print("<body>", file=f)
+        print("<h1>{0}</h1>".format(e['origin']['title']), file=f)
+        print("""<h2><a href="{0}">{1}</a></h2>""".format(e['alternate'][0]['href'], e['title']), file=f)
         which = "content" if "content" in e else "summary"
         print(e[which]['content'], file=f)
         print("</body>", file=f)
         print("</html>", file=f)
         f.close()
     f = open(os.path.join(dir, "reader.opf"), "w")
-    print("""<?xml version="1.0"?>
+    print("""<?xml version="1.0" encoding="UTF-8"?>
     <package unique-identifier="uid" xmlns:dc="Dublin Core">
         <metadata>
             <dc-metadata>
@@ -40,7 +43,7 @@ try:
         </metadata>
         <manifest>""", file=f)
     for e in feed['items']:
-        print("""<item id="{0}" href="{0}" media-type="text/html" />""".format(e['id'].replace("/", "_")), file=f)
+        print("""<item id="{0}" href="{0}.html" media-type="text/html" />""".format(e['id'].replace("/", "_")), file=f)
     print("""</manifest>
         <spine>""", file=f)
     for e in feed['items']:
