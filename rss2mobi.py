3180f102197cb11ead73217e8c3fadabfc82d030
Greg Hewgill <greg@hewgill.com>
2011-05-21 21:01:27 +1200

    handle case where no title appears

--- rss2mobi.py
+++ rss2mobi.py
@@ -136,6 +136,8 @@ try:
         #pprint.pprint(e)
         fname = nameify(e['id']) + ".html"
         e['fname'] = fname
+        if 'title' not in e:
+            e['title'] = "(no title)"
         f = open(os.path.join(dir, fname), "w", encoding="utf-8")
         print("""<?xml version="1.0" encoding="UTF-8"?>""", file=f)
         print("""<html xmlns="http://www.w3.org/1999/xhtml">""", file=f)
